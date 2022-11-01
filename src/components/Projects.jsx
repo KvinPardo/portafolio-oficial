588cd9bf9373f7ad096d38e6a55cfd5069f2a00f
KvinPardo <kevin.pardov25@gmail.com>
2022-11-01 17:32:43 -0300

    cambio-formulario

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Tareas from "../assets/adm-tareas.png";
-import Clima from "../assets/app-clima.png";
+import Formulario from "../assets/formulario.png";
 import BienesRaices from "../assets/bienes-raices.png";
 import PaginaVentas from "../assets/pagina-ventas.png";
 import Pagina from "../assets/landing-page.png";
@@ -33,12 +33,12 @@ const Projects = () => {
                 Administrador de Tareas
               </span>
               <div className="pt-8 text-center">
-                <a href="/">
+                <a href="https://adm-tareas-react.netlify.app/" target="_blank">
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Demo
                   </button>
                 </a>
-                <a href="/">
+                <a href="https://github.com/KvinPardo/adm-tareas" target="_blank">
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Código
                   </button>
@@ -47,21 +47,21 @@ const Projects = () => {
             </div>
           </div>
           <div
-            style={{ backgroundImage: `url(${Clima})` }}
+            style={{ backgroundImage: `url(${Formulario})` }}
             className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
           >
             {/* Hover Effects */}
             <div className="opacity-0 group-hover:opacity-100">
               <span className="text-2xl font-bold text-white tracking-wider">
-                Aplicación de Clima
+                Formulario - Login
               </span>
               <div className="pt-8 text-center">
-                <a href="/">
+                <a href="" target="_blank">
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Demo
                   </button>
                 </a>
-                <a href="/">
+                <a href="https://github.com/KvinPardo/clima-app-API">
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Código
                   </button>
